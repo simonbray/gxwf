ec69e0d64bf9f1b5491fd89a2a20567e169264c0
Simon Bray <sbray1371@gmail.com>
2020-03-05 10:54:48 +0100

    add tabulate function

--- gxwf/cli.py
+++ gxwf/cli.py
@@ -44,13 +44,42 @@ def _login():
     try:
         with open(CONFIG_PATH) as f: 
             cnfg = yaml.safe_load(f) 
-            gi = galaxy.GalaxyInstance(cnfg['url'], cnfg['api_key'])
-            gi.histories.get_histories()  # just to check the connection
+        gi = galaxy.GalaxyInstance(cnfg['url'], cnfg['api_key'])
+        gi.histories.get_histories()  # just to check the connection
         return gi, cnfg
     except FileNotFoundError:
         print("No login details provided - please run gxwf init")
     except ConnectionError:
         print("Could not connect - check login details are correct.")
+
+
+def _tabulate(values):
+    """
+    Print data as a table
+
+    values is a list of lists, each list a column
+    """
+    try:
+        width = os.get_terminal_size(0)[0]  # get terminal width
+    except OSError:
+        width = 80  # default
+
+    col_widths = [len(max(col, key=len)) + 2 for col in values]
+
+    if sum(col_widths) > width:  # check if the columns are too wide for terminal
+        wide_col = col_widths.index(max(col_widths))  # for simplicity we only edit the widest col
+        col_widths[wide_col] -= sum(col_widths) - width
+        for n in range(len(values[wide_col])):  # check elements of the widest col
+            val = values[wide_col][n]
+            if len(val) > col_widths[wide_col] - 5:  # insert ellipsis to shorten wide elements
+                values[wide_col][n] = val[:int(col_widths[wide_col]/2-3)] + '...' + val[int(3-col_widths[wide_col]/2):]
+
+    row_format = ''.join(["{{:<{}}}".format(n) for n in col_widths])
+
+    click.echo(click.style(row_format.format(*[col[0] for col in values]), bold=True))  # print col headers
+    for row in range(1, len(values[0])):
+        click.echo(row_format.format(*[col[row] for col in values]))
+
 
 class Info(object):
     """
@@ -129,8 +158,11 @@ def init(url, api_key):
         if overwrite not in ['Y', 'y']:
             return 0
     try:
-        gi = galaxy.GalaxyInstance(url, api_key)
-        hid = gi.histories.create_history(name='GXWF datasets')['id']
+        gi = galaxy.GalaxyInstance(url=url, key=api_key)
+        gi.histories.create_history(name='test')['id']
+        # print(gi.histories.create_history(name='GXWF datasets'))
+        # hid = gi.histories.create_history(name='GXWF datasets')['id']
+        #   gi.histories.create_history(name='test')['id']
     except ConnectionError as e:
         click.echo("Accessing server failed with '{}'".format(e))
     else:
@@ -153,10 +185,19 @@ def list(public, search):
     else:
         workflows = gi.workflows.get_workflows(published=public)
 
+    wf_name, wf_id, steps, owner = ['Workflow name'], ['ID'], ['Steps'], ['Owner']
 
-    click.echo(click.style("{:>100}{:>30}{:>10}{:>20}".format('Workflow name', 'ID', 'Steps', 'Owner'), bold=True))
     for wf in workflows:
-        click.echo("{:>100}{:>30}{:>10}{:>20}".format(wf['name'], wf['id'], wf['number_of_steps'], wf['owner']))
+        wf_name.append(wf['name'])
+        wf_id.append(wf['id'])
+        steps.append(str(wf['number_of_steps']))
+        owner.append(wf['owner'])
+
+    _tabulate([wf_name, wf_id, steps, owner])
+
+    # click.echo(click.style("{:>100}{:>30}{:>10}{:>20}".format('Workflow name', 'ID', 'Steps', 'Owner'), bold=True))
+    # for wf in workflows:
+    #     click.echo("{:>100}{:>30}{:>10}{:>20}".format(wf['name'], wf['id'], wf['number_of_steps'], wf['owner']))
 
 
 @cli.command()
